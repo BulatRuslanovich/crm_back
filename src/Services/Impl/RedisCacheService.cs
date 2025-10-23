2cf7b569eb607474bfc410734f7e4cd1bbdc82c7
getname <bulatruslanovich@gmail.com>
2025-10-23 22:04:36 +0300

    refactor: update RedisCacheService

--- src/Services/Impl/RedisCacheService.cs
+++ src/Services/Impl/RedisCacheService.cs
@@ -1,13 +1,10 @@
 using System.Text.Json;
 using Microsoft.Extensions.Caching.Distributed;
-using StackExchange.Redis;
 
 namespace CrmBack.Services.Impl;
 
-public class RedisCacheService(IDistributedCache cache, IConnectionMultiplexer redis) : IRedisCacheService
+public class RedisCacheService(IDistributedCache cache) : IRedisCacheService
 {
-    private readonly IDatabase _database = redis.GetDatabase();
-
     public async Task<T?> GetAsync<T>(string key, CancellationToken ct = default)
     {
         var cachedData = await cache.GetStringAsync(key, ct);
@@ -26,27 +23,5 @@ public class RedisCacheService(IDistributedCache cache, IConnectionMultiplexer r
     public async Task RemoveAsync(string key, CancellationToken ct = default)
     {
         await cache.RemoveAsync(key, ct);
-    }
-
-    public async Task RemoveByPatternAsync(string pattern, CancellationToken ct = default)
-    {
-        var keys = await GetKeysByPatternAsync(pattern, ct);
-        if (keys.Count > 0)
-        {
-            await _database.KeyDeleteAsync([.. keys.Select(k => (RedisKey)k)]);
-        }
-    }
-
-    public async Task<List<string>> GetKeysByPatternAsync(string pattern, CancellationToken ct = default)
-    {
-        var keys = new List<string>();
-        var server = redis.GetServer(redis.GetEndPoints().First());
-
-        await foreach (var key in server.KeysAsync(pattern: pattern))
-        {
-            keys.Add(key.ToString());
-        }
-
-        return keys;
     }
 }
